625be5bba95d61afab82da6919fe25c2bef9218b
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-15 10:42:07 -0400

    Complete Appointment show page

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,7 @@
 class AppointmentsController < ApplicationController
+  def show
+    @appointment = Appointment.find(params[:id])
+    @doctor = Doctor.find(params[:id])
+    @patient = Patient.find(params[:id])
+  end
 end
